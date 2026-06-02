f0c6ddbff02c31fe02865c50b7ae3f35659cebd9
thunderkite <hanasisgenya200710@gmail.com>
2026-06-02 23:18:20 +0300

    Update test_login.py

--- python/test_login.py
+++ python/test_login.py
@@ -2,7 +2,6 @@ import time
 from appium import webdriver
 from appium.options.common import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
-# Импортируем модули для умного ожидания элементов:
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -17,12 +16,10 @@ options.set_capability('noReset', False)
 print("Подключаемся к Appium серверу и запускаем приложение...")
 driver = webdriver.Remote('http://localhost:4723', options=options)
 
-# Настраиваем объект ожидания: максимум ждем 15 секунд появления элемента
 wait = WebDriverWait(driver, 15)
 
 try:
     print("Ожидаем появления поля ввода телефона на экране...")
-    # Ищем поле телефона по твоему полному XPath
     phone_field = wait.until(
         EC.presence_of_element_located((
             AppiumBy.XPATH, '//android.widget.EditText[@resource-id="phone"]'
@@ -34,7 +31,6 @@ try:
     phone_field.clear()
     phone_field.send_keys("+79160000071")
 
-    # Ищем поле пароля
     print("Ожидаем появления поля пароля...")
     password_field = wait.until(
         EC.presence_of_element_located((
@@ -47,12 +43,10 @@ try:
     password_field.clear()
     password_field.send_keys("Qwerty100")
 
-    # Скрываем клавиатуру
     if driver.is_keyboard_shown():
         print("Скрываем клавиатуру...")
         driver.hide_keyboard()
 
-    # НАЖАТИЕ НА КНОПКУ "ВОЙТИ"
     print("Ожидаем появления кнопки 'Войти'...")
     login_button = wait.until(
         EC.element_to_be_clickable((
@@ -63,7 +57,6 @@ try:
     print("Кнопка найдена! Нажимаем 'Войти'...")
     login_button.click()
     
-    # Даем приложению загрузить главный экран после входа
     print("Ожидаем переход на главный экран...")
     time.sleep(7)
     print("Тест авторизации успешно завершен!")
